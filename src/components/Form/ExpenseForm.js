5df0c9416beb8fc944939cb84bfcd4a4737783c1
manish kumar <manishkaswan5@gmail.com>
2024-01-26 09:30:26 +0530

    style the form

--- src/components/Form/ExpenseForm.js
+++ src/components/Form/ExpenseForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ExpenseForm.css";
 function ExpenseForm() {
     function titleChange(evt) {
         console.log(evt.target.value);
@@ -12,19 +13,28 @@ function ExpenseForm() {
 
 
     return (
+        <div className="new-expense">
 
         <form id="expenseForm">
+            <div className="new-expense__controls">
+            <div className="new-expense_control">
             <label for="expenseTitle">Expense Title:</label>
             <input onChange={titleChange} type="text" id="expenseTitle" name="expenseTitle" required />
-
+            </div>
+            <div className="new-expense_control">
             <label for="expenseAmount">Expense Amount:</label>
             <input onChange={ammountChange} type="number" id="expenseAmount" name="expenseAmount" required />
-
+            </div>
+            <div className="new-expense_control">
             <label for="expenseDate">Date:</label>
             <input onChange={DateChange} type="date" id="expenseDate" name="expenseDate" required />
-
-            <button type="button">Submit</button>
+            </div>
+            </div>
+            <div className="new-expense__actions">
+            <button type="Submit">Add Expense</button>
+            </div>
         </form>
+        </div>
     )
 }
 
