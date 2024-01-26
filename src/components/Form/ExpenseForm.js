8fc29a580c9ea57f1cab2513af301b990f601b3b
manish kumar <manishkaswan5@gmail.com>
2024-01-26 09:52:19 +0530

    add usestate at form fields chenges

--- src/components/Form/ExpenseForm.js
+++ src/components/Form/ExpenseForm.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import "./ExpenseForm.css";
+
 function ExpenseForm() {
+
+    const [enteredtitle,setenteredtitle] = useState('')
+    const [enteredAmount,setenteredamount] = useState('')
+    const [enteredDate,setenteredDate] = useState('')
+
     function titleChange(evt) {
+        setenteredtitle(evt.target.value);
         console.log(evt.target.value);
     }
     function ammountChange(evt) {
+        setenteredamount(evt.target.value);
         console.log(evt.target.value);
     }
     function DateChange(evt) {
+        setenteredDate(evt.target.value);
         console.log(evt.target.value);
     }
+
+    
+
 
 
     return (
@@ -19,15 +31,15 @@ function ExpenseForm() {
             <div className="new-expense__controls">
             <div className="new-expense_control">
             <label for="expenseTitle">Expense Title:</label>
-            <input onChange={titleChange} type="text" id="expenseTitle" name="expenseTitle" required />
+            <input onChange={titleChange} type="text" id="expenseTitle" name="expenseTitle" required ></input>
             </div>
             <div className="new-expense_control">
             <label for="expenseAmount">Expense Amount:</label>
-            <input onChange={ammountChange} type="number" id="expenseAmount" name="expenseAmount" required />
+            <input onChange={ammountChange} type="number" id="expenseAmount" name="expenseAmount" required ></input>
             </div>
             <div className="new-expense_control">
             <label for="expenseDate">Date:</label>
-            <input onChange={DateChange} type="date" id="expenseDate" name="expenseDate" required />
+            <input onChange={DateChange} type="date" id="expenseDate" name="expenseDate" required ></input>
             </div>
             </div>
             <div className="new-expense__actions">
